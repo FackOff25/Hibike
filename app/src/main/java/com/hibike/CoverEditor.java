89011f22b7640444a1d06a1afba5cff149827e4e
FackOff25 <kozvladimir@bk.ru>
2021-09-23 18:14:48 +0300

    Made Song class, Playlist, SongButton, MusicData, Exceptions

---
+++ app/src/main/java/com/hibike/CoverEditor.java
@@ -0,0 +1,14 @@
+package com.hibike;
+
+public class CoverEditor {
+
+    //Makes images with 100px on one dimension from image it got
+    static String shrink(String path){
+
+    }
+
+    //Makes muted images from images it got
+    static String mute(String path){
+
+    }
+}
